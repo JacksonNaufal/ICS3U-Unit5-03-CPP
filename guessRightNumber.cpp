e32fa9ee011f618d7c07e677bafb105e688e4186
Jackson Naufal <Jackson.naufal@mths.ca>
2022-05-11 17:43:54 +0000

    FixedCode

--- guessRightNumber.cpp
+++ guessRightNumber.cpp
@@ -5,32 +5,60 @@
 
 #include <iostream>
 
-void AreaConversion(int length, int width) {
-     int area;
+void GradeConversion(std::string gradeLevel) {
+     float gradePercentage;
 
-        area = (length * width) / 2;
-        std::cout << "The area is "<< area << "! The Height = " << length <<
-        " The Base = " << width << std::endl;
-}
-main() {
-    std::string WidthUserString;
-    std::string LengthUserString;
-    int WidthUser;
-    int LengthUser;
-
-        // input
-    std::cout << "Enter your width (mm)!: ";
-    std::cin >> WidthUserString;
-    std::cout << "Enter your length! (mm): ";
-    std::cin >> LengthUserString;
-
-    try {
-        WidthUser = std::stoi(WidthUserString);
-        LengthUser = std::stoi(LengthUserString);
-        AreaConversion(WidthUser, LengthUser);
-    } catch (std::invalid_argument) {
-        std::cout << "\n Invalid integer!" << std::endl;
+     if (gradeLevel == "4+") {
+        gradePercentage = 97.5;
+     } else if (gradeLevel == "4") {
+        gradePercentage = 90.5;
+     } else if (gradeLevel == "4-") {
+        gradePercentage = 83.0;
+     } else if (gradeLevel == "3+") {
+        gradePercentage = 78.0;
+     } else if (gradeLevel == "3") {
+        gradePercentage = 74.5;
+     } else if (gradeLevel == "3-") {
+        gradePercentage = 71.0;
+     } else if (gradeLevel == "2+") {
+        gradePercentage = 68.0;
+     } else if (gradeLevel == "2") {
+        gradePercentage = 65.5;
+     } else if (gradeLevel == "2-") {
+        gradePercentage = 61.0;
+     } else if (gradeLevel == "1+") {
+        gradePercentage = 58.0;
+     } else if (gradeLevel == "1") {
+        gradePercentage = 54.5;
+     } else if (gradeLevel == "1-") {
+        gradePercentage = 51.0;
+     } else if (gradeLevel == "R") {
+        gradePercentage = 39.5;
+    } else {
+        gradePercentage = -1.0;
     }
 
-    std::cout << "\nDone." << std::endl;
+        return gradePercentage;
+}
+
+
+main() {
+    
+    std::string userLevel;
+    float convertedLevel;
+
+        // input
+    std::cout << "Enter your grade!: ";
+    std::cin >> userLevel;
+
+    convertedLevel = GradeConversion(userLevel);
+
+    if (convertedLevel == -1) {
+        std::cout << "Invalid Input";
+    } else {
+        std::cout << "The level" << userLevel << "is a " 
+        << convertedLevel << std::endl;
+    }
+
+        std::cout << "\nDone." << std::endl;
 }
